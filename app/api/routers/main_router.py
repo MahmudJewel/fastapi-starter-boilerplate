1cf33be502c002fa9fef26469efd6d38f5291d57
Mahmud Jewel <jmbengal.mbstu@gmail.com>
2026-04-19 18:08:20 +0600

    feat: add project working indicator

--- app/api/routers/main_router.py
+++ app/api/routers/main_router.py
@@ -3,6 +3,10 @@ from app.api.routers.user import user_router
 
 router = APIRouter()
 
+@router.get('/')
+async def home_page():
+    return {"msg": "API is working fine"}
+
 router.include_router(user_router)
 
 
